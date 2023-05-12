545ebf7b84f43f4b070d50934bed14bb97e414ca
davi-i <josedavifrancelino@gmail.com>
2023-05-12 08:39:07 -0300

    feat: only two players at a time

--- src/server.js
+++ src/server.js
@@ -6,13 +6,41 @@ const crypto = require('crypto');
 
 app.use(express.static('public'));
 
-const players = [];
+let leftPlayer = null;
+let rightPlayer = null;
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
+    if (leftPlayer == socket) {
+      leftPlayer = null;
+    } else if (rightPlayer == socket) {
+      rightPlayer = null;
+    }
+  });
+
+  function enterGame(side) {
+    if (side == 'left') {
+      if (leftPlayer) {
+        return 1;
+      }
+      leftPlayer = socket;
+    } else if (side == 'right') {
+      if (rightPlayer) {
+        return 1;
+      }
+      rightPlayer = socket;
+    } else {
+      return 2;
+    }
+    return 0;
+  }
+
+  socket.on('enterGame', (side) => {
+    code = enterGame(side);
+    socket.emit('enterGame', { side, code });
   });
 
   socket.on('updatePaddle', (data) => {
@@ -21,9 +49,9 @@ io.on('connection', (socket) => {
     } else if (data.player === 'right') {
       rightPaddleY = data.y;
     }
-    socket.broadcast.emit('updatePaddle', data);
+    io.emit('updatePaddle', data);
   });
-  
+
 });
 
 const port = process.env.PORT || 3000;
